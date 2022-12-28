8fc1775835214dc0d8ae1ff348e7ab73184661b5
suryaa6666 <suryaelidanto@gmail.com>
2022-12-28 19:00:36 +0700

    result

--- lib/main.dart
+++ lib/main.dart
@@ -8,34 +8,48 @@ void main() {
 class MyApplication extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // final List<Color> myColor = [
+    //   Colors.blue,
+    //   Colors.black,
+    //   Colors.green,
+    //   Colors.purple
+    // ];
+
+    List<Widget> myList = List.generate(
+        100,
+        (index) => Text(
+              "${index + 1}",
+              style: TextStyle(fontSize: 5 + double.parse(index.toString())),
+            ));
+
+    // List<Container> data = [
+    //   Container(
+    //     height: 200,
+    //     width: 200,
+    //     color: Colors.green,
+    //   ),
+    //   Container(
+    //     height: 200,
+    //     width: 200,
+    //     color: Colors.blue,
+    //   ),
+    //   Container(
+    //     height: 200,
+    //     width: 200,
+    //     color: Colors.amber,
+    //   ),
+    //   Container(
+    //     height: 200,
+    //     width: 200,
+    //     color: Colors.red,
+    //   )
+    // ];
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Scaffold(
-          appBar: AppBar(title: Text("Invisible Widget")),
-          body: Stack(
-            children: [
-              Container(
-                height: 400,
-                width: 400,
-                color: Colors.green,
-              ),
-              Container(
-                height: 300,
-                width: 300,
-                color: Colors.blue,
-              ),
-              Container(
-                height: 200,
-                width: 200,
-                color: Colors.amber,
-              ),
-              Container(
-                height: 100,
-                width: 100,
-                color: Colors.red,
-              )
-            ],
-          ),
-        ));
+            appBar: AppBar(title: Text("List View")),
+            body: ListView(
+              children: myList,
+            )));
   }
 }
