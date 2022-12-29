fdc0cc4d13f037edcdc658eacf266b1602aba7d1
suryaa6666 <suryaelidanto@gmail.com>
2022-12-29 13:41:16 +0700

    result

--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,69 @@
-import 'package:faker/faker.dart';
+import 'dart:developer';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApplication());
+  runApp(MyApp());
 }
 
-class MyApplication extends StatelessWidget {
-  var faker = new Faker();
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int counter = 1;
 
   @override
   Widget build(BuildContext context) {
-    // var list = [
-    //   ChatItem(
-    //     imageUrl: "https://picsum.photos/200/300",
-    //     name: faker.person.name(),
-    //     message: faker.conference.name(),
-    //     clock: faker.date.justTime(),
-    //   ),
-    // ];
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-            appBar: AppBar(title: Text("Dummy Chat")),
-            body: ListView.builder(
-              itemCount: 100,
-              itemBuilder: (context, index) => ChatItem(
-                imageUrl: "https://picsum.photos/id/${index}/200/300",
-                name: faker.person.name(),
-                message: faker.conference.name(),
-                clock: faker.date.justTime(),
-              ),
-            )));
-  }
-}
-
-class ChatItem extends StatelessWidget {
-  String imageUrl;
-  String name;
-  String message;
-  String clock;
-
-  ChatItem({
-    this.imageUrl = "",
-    this.name = "",
-    this.message = "",
-    this.clock = "",
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ListTile(
-          contentPadding: EdgeInsets.all(10),
-          leading: CircleAvatar(
-            backgroundImage: NetworkImage(imageUrl),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Counter Sederhana! 😎✌",
           ),
-          title: Text(name),
-          subtitle: Text(message, maxLines: 1, overflow: TextOverflow.ellipsis),
-          trailing: Text(clock),
         ),
-        Divider(
-          color: Colors.black,
-          thickness: 2,
-        )
-      ],
+        body: Column(children: [
+          Center(
+            child: Container(
+                margin: EdgeInsets.only(top: 50),
+                child: Column(
+                  children: [
+                    Text(
+                      counter.toString(),
+                      style: TextStyle(fontSize: 50 + counter.toDouble()),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 100),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          ElevatedButton(
+                              onPressed: () {
+                                setState(() {
+                                  counter++;
+                                });
+                                log(counter.toString());
+                              },
+                              child: Icon(Icons.add)),
+                          ElevatedButton(
+                              onPressed: () {
+                                setState(() {
+                                  counter--;
+                                });
+                                log(counter.toString());
+                              },
+                              child: Icon(Icons.remove))
+                        ],
+                      ),
+                    ),
+                  ],
+                )),
+          )
+        ]),
+      ),
     );
   }
 }
