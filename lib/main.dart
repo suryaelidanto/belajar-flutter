989b8c30bee2d8d9aa517b2fbd8d75830b52e2c4
suryaa6666 <suryaelidanto@gmail.com>
2022-12-30 10:30:25 +0700

    very cool yooo

--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,116 @@
-import 'package:faker/faker.dart';
+import 'dart:developer';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
-  runApp(MyApplication());
+  runApp(MyApp());
 }
 
-class MyApplication extends StatelessWidget {
-  var faker = new Faker();
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  final List<Map<String, dynamic>> myList = [
+    {
+      "name": "Cintara Surya Elidanto",
+      "age": "99",
+      "genre": [
+        "smart",
+        "cool",
+        "funny",
+        "sip mantap",
+        "okelah",
+        "coba",
+        "123123",
+        "asdsad"
+      ]
+    },
+    {
+      "name": "Surya Elz",
+      "age": "98",
+      "genre": ["okelah", "amazing", "sip sip"]
+    },
+    {
+      "name": "Okelah",
+      "age": "76",
+      "genre": ["mantap", "okesip", "nice"]
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
-    // var list = [
-    //   ChatItem(
-    //     imageUrl: "https://picsum.photos/200/300",
-    //     name: faker.person.name(),
-    //     message: faker.conference.name(),
-    //     clock: faker.date.justTime(),
-    //   ),
-    // ];
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Scaffold(
-            appBar: AppBar(title: Text("Extract Widget")),
-            body: ListView.builder(
-              itemCount: 100,
-              itemBuilder: (context, index) => ChatItem(
-                imageUrl: "https://picsum.photos/id/${index}/200/300",
-                name: faker.person.name(),
-                message: faker.conference.name(),
-                clock: faker.date.justTime(),
-              ),
-            )));
-  }
-}
-
-class ChatItem extends StatelessWidget {
-  String imageUrl;
-  String name;
-  String message;
-  String clock;
-
-  ChatItem({
-    this.imageUrl = "",
-    this.name = "",
-    this.message = "",
-    this.clock = "",
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ListTile(
-          contentPadding: EdgeInsets.all(10),
-          leading: CircleAvatar(
-            backgroundImage: NetworkImage(imageUrl),
+          appBar: AppBar(title: Text("Mapping List")),
+          body: ListView(
+            padding: EdgeInsets.all(10),
+            children: myList.map((data) {
+              List genre = data["genre"];
+              return ElevatedButton(
+                onPressed: () {
+                  log(data["name"]);
+                },
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.white,
+                    onPrimary: Colors.black.withOpacity(0.8)),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        CircleAvatar(
+                            backgroundImage:
+                                NetworkImage("https://picsum.photos/200/300")),
+                        Container(
+                          margin: EdgeInsets.only(left: 10),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Name : ${data["name"]}",
+                                style: TextStyle(
+                                    fontSize: 20, fontFamily: "Poppins"),
+                              ),
+                              Text(
+                                "Age : ${data["age"]}",
+                                style: TextStyle(
+                                    fontSize: 20, fontFamily: "Poppins"),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 20),
+                      child: SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        child: Row(
+                            children: genre.map((genreItem) {
+                          return Container(
+                            margin: EdgeInsets.symmetric(horizontal: 10),
+                            color: Colors.blue,
+                            padding: EdgeInsets.all(10),
+                            child: Text(
+                              "${genreItem}",
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  fontFamily: "Poppins",
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white),
+                            ),
+                          );
+                        }).toList()),
+                      ),
+                    ),
+                    Divider(),
+                  ],
+                ),
+              );
+            }).toList(),
           ),
-          title: Text(name),
-          subtitle: Text(message, maxLines: 1, overflow: TextOverflow.ellipsis),
-          trailing: Text(clock),
-        ),
-        Divider(
-          color: Colors.black,
-          thickness: 2,
-        )
-      ],
-    );
+        ));
   }
 }
