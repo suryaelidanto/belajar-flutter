ddbc392d1b495022558eeb9cc0627621dceb9345
suryaa6666 <suryaelidanto@gmail.com>
2022-12-30 10:52:44 +0700

    result wowwo mantap

--- lib/main.dart
+++ lib/main.dart
@@ -1,116 +1,27 @@
-import 'dart:developer';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
+import 'package:intl/intl.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MyApplication());
 }
 
-class MyApp extends StatelessWidget {
-  MyApp({super.key});
-
-  final List<Map<String, dynamic>> myList = [
-    {
-      "name": "Cintara Surya Elidanto",
-      "age": "99",
-      "genre": [
-        "smart",
-        "cool",
-        "funny",
-        "sip mantap",
-        "okelah",
-        "coba",
-        "123123",
-        "asdsad"
-      ]
-    },
-    {
-      "name": "Surya Elz",
-      "age": "98",
-      "genre": ["okelah", "amazing", "sip sip"]
-    },
-    {
-      "name": "Okelah",
-      "age": "76",
-      "genre": ["mantap", "okesip", "nice"]
-    },
-  ];
-
+class MyApplication extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          appBar: AppBar(title: Text("Mapping List")),
-          body: ListView(
-            padding: EdgeInsets.all(10),
-            children: myList.map((data) {
-              List genre = data["genre"];
-              return ElevatedButton(
-                onPressed: () {
-                  log(data["name"]);
-                },
-                style: ElevatedButton.styleFrom(
-                    primary: Colors.white,
-                    onPrimary: Colors.black.withOpacity(0.8)),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        CircleAvatar(
-                            backgroundImage:
-                                NetworkImage("https://picsum.photos/200/300")),
-                        Container(
-                          margin: EdgeInsets.only(left: 10),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                "Name : ${data["name"]}",
-                                style: TextStyle(
-                                    fontSize: 20, fontFamily: "Poppins"),
-                              ),
-                              Text(
-                                "Age : ${data["age"]}",
-                                style: TextStyle(
-                                    fontSize: 20, fontFamily: "Poppins"),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                    Container(
-                      margin: EdgeInsets.symmetric(vertical: 20),
-                      child: SingleChildScrollView(
-                        scrollDirection: Axis.horizontal,
-                        child: Row(
-                            children: genre.map((genreItem) {
-                          return Container(
-                            margin: EdgeInsets.symmetric(horizontal: 10),
-                            color: Colors.blue,
-                            padding: EdgeInsets.all(10),
-                            child: Text(
-                              "${genreItem}",
-                              style: TextStyle(
-                                  fontSize: 20,
-                                  fontFamily: "Poppins",
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white),
-                            ),
-                          );
-                        }).toList()),
-                      ),
-                    ),
-                    Divider(),
-                  ],
-                ),
-              );
-            }).toList(),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Date Format"),
+        ),
+        body: Center(
+          child: Container(
+            child: Text(DateFormat.yMMMEd().add_Hms().format(DateTime.now()),
+                style: TextStyle(fontSize: 20, fontFamily: "Poppins")),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
