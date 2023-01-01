bd59130231b5c050b75c6e0b3c07f1cd8347998d
suryaa6666 <suryaelidanto@gmail.com>
2023-01-01 12:57:14 +0700

    result

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,56 +11,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<Tab> myTab = [
-      Tab(
-        icon: Icon(Icons.home),
-      ),
-      Tab(
-        icon: Icon(Icons.camera),
-      ),
-      Tab(
-        icon: Icon(Icons.settings),
-      ),
-    ];
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: DefaultTabController(
-        initialIndex: 2,
-        length: myTab.length,
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text("Tabbar Widget"),
-            bottom: PreferredSize(
-              preferredSize: Size.fromHeight(50),
-              child: TabBar(
-                labelColor: Colors.black,
-                unselectedLabelColor: Colors.white,
-                unselectedLabelStyle: TextStyle(),
-                // indicatorColor: Colors.yellow,
-                // indicatorWeight: 5,
-                // indicatorPadding: EdgeInsets.all(10),
-                indicator: BoxDecoration(
-                  color: Colors.amber,
-                  border:
-                      Border(bottom: BorderSide(color: Colors.black, width: 5)),
-                ),
-                tabs: myTab,
-              ),
-            ),
-          ),
-          body: TabBarView(children: [
-            Center(
-              child: Text("Ini halaman pertama 😎"),
-            ),
-            Center(
-              child: Text("Ini halaman kedua 😂"),
-            ),
-            Center(
-              child: Text("Ini halaman ketiga 😁"),
-            ),
-          ]),
-        ),
+      home: Scaffold(
+        appBar: AppBar(title: Text("Fitur Text Field")),
+        body: Center(
+            child: TextField(
+          autocorrect: true,
+          autofocus: true,
+          enableSuggestions: true,
+          enableInteractiveSelection: false,
+          enabled: true,
+          obscureText: true,
+          obscuringCharacter: "+",
+          keyboardType: TextInputType.number,
+        )),
       ),
     );
   }
